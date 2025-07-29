44ff0704b276b317fd4c3391000aeaf963faf112
GiantbYTES <schakedamir@gmail.com>
2025-07-29 17:28:27 +0300

    add model with getuUser and get Friends functions

---
+++ Week 5/RUPG Project/model.js
@@ -0,0 +1,48 @@
+async function getUser() {
+  try {
+    const res = await fetch(`https://randomuser.me/api/`);
+    if (!res.ok) {
+      throw new Error("User not created");
+    }
+    const data = await res.json();
+    const picture = data.results[0].picture.large;
+    const firstName = data.results[0].name.first;
+    const lastName = data.results[0].name.last;
+    const city = data.results[0].location.city;
+    const state = data.results[0].location.state;
+    const mainUser = {
+      picture,
+      fName: firstName,
+      lName: lastName,
+      city,
+      state,
+    };
+    console.log(mainUser);
+    return mainUser;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getFriends() {
+  try {
+    const friends = [];
+    for (let i = 0; i < 6; i++) {
+      const res = await fetch(`https://randomuser.me/api/`);
+      if (!res.ok) {
+        throw new Error("Friend not created");
+      }
+      const data = await res.json();
+      const firstName = data.results[0].name.first;
+      const lastName = data.results[0].name.last;
+      friends.push({ fName: firstName, lName: lastName });
+    }
+    console.log(friends);
+    return friends;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+getUser();
+getFriends();
