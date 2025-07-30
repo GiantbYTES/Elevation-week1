0b526e326b8f5e8cd4e2727f4ef810647b8af059
GiantbYTES <schakedamir@gmail.com>
2025-07-30 10:58:30 +0300

    add a controller

--- Week 5/RUPG Project/main.js
+++ Week 5/RUPG Project/main.js
@@ -1,5 +1,16 @@
 import Renderer from "./render.js";
 import User from "./model.js";
 
-const user = User();
+const user = await User();
 const renderer = Renderer();
+
+const profile = await user.getUser();
+const friends = await user.getFriends();
+const quote = await user.getQuote();
+const pokemon = await user.getPokemon();
+const about = await user.getText();
+
+renderer.renderPage(profile, quote, pokemon, about, friends);
+
+// console.log(profile);
+// renderer.renderPage(profile);
