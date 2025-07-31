bec8916a6fa95dd662bf533ca1cc136d428e21a7
GiantbYTES <schakedamir@gmail.com>
2025-07-31 11:05:22 +0300

    add model

--- Week 5/Stock Exchange Project/src/model.js
+++ Week 5/Stock Exchange Project/src/model.js
@@ -2,13 +2,26 @@ require("dotenv").config();
 
 const apiKey = process.env.API_KEY;
 
-async function getCompanies(input) {
-  try {
-    const res = await fetch(
-      `https://financialmodelingprep.com/api/v3/search?query=${input}&limit=10&exchange=NASDAQ&apikey=${apiKey}`
-    );
-    const data = res.json();
-  } catch (error) {
-    console.log(error.message);
+async function Model() {
+  async function getCompanies(input) {
+    const loader = document.getElementById("loader");
+    loader.style.display = "block";
+    try {
+      const res = await fetch(
+        `https://financialmodelingprep.com/api/v3/search?query=${input}&limit=10&exchange=NASDAQ&apikey=${apiKey}`
+      );
+      if (!res.ok) {
+        throw new Error("Can't reach data");
+      }
+      const data = await res.json();
+      console.log("data: " + JSON.stringify(data));
+      return data;
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      loader.style.display = "none";
+    }
   }
+  return { getCompanies };
 }
+export default Model;
