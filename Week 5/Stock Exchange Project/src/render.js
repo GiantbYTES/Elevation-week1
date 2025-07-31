7988beabf80c1a9e5eaf230a0cfa607250944346
GiantbYTES <schakedamir@gmail.com>
2025-07-31 11:05:40 +0300

    add rendercompanylist

--- Week 5/Stock Exchange Project/src/render.js
+++ Week 5/Stock Exchange Project/src/render.js
@@ -1,6 +1,15 @@
 function Renderer() {
   function renderCompanyList(companies) {
-    companies.forEach((c) => {});
+    const companyList = document.getElementById("company-list");
+    console.log("companies: " + companies);
+    let id = 1;
+    companies.forEach((c) => {
+      $(companyList).append(
+        `<li class="company-name"><a href="/company.html?symbol=${c.symbol}">${c.name} (${c.symbol})</a></li>`
+      );
+    });
   }
   return { renderCompanyList };
 }
+
+export default Renderer;
