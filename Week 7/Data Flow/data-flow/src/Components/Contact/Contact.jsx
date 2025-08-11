a5566270832888c3268212b769093ccc29fc4a07
GiantbYTES <schakedamir@gmail.com>
2025-08-11 15:27:10 +0300

    add Ex4

---
+++ Week 7/Data Flow/data-flow/src/Components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import "./Contact.css";
+
+export function Contact({ contact }) {
+  return (
+    <div className="Contact">
+      <p>{contact}</p>
+    </div>
+  );
+}
+
+export default Contact;
--- Week 7/Data Flow/data-flow/src/Components/List/List.jsx
+++ Week 7/Data Flow/data-flow/src/Components/List/List.jsx
@@ -1,9 +1,13 @@
 import "./List.css";
+import Contact from "../Contact/Contact";
 
 export function List({ contacts }) {
+  //   console.log(contacts);
   return (
     <div className="List">
-      <p>List Component</p>
+      {contacts.map((c, id) => (
+        <Contact key={id} contact={c} />
+      ))}
     </div>
   );
 }
